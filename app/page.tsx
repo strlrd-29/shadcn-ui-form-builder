fc5b7f3146f8f5b51f53b342eedfd7e279066990
Ouassim Abdelmalek Ghribi <ouassimghribi@gmail.com>
2024-10-29 08:13:10 +0100

    feat: prepared form preview design

--- app/page.tsx
+++ app/page.tsx
@@ -33,9 +33,25 @@ export default function Home() {
             </Breadcrumb>
           </div>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4">
-          <div className="mx-auto h-24 w-full max-w-3xl rounded-xl bg-muted/50" />
-          <div className="mx-auto h-[100vh] w-full max-w-3xl rounded-xl bg-muted/50" />
+        <div className="mx-auto flex w-full max-w-4xl flex-col gap-4 rounded-md border shadow-sm">
+          <div className="flex items-center gap-1.5 border-b p-4">
+            <div
+              className="h-2.5 w-2.5 rounded-full bg-red-500"
+              data-v-241c1152=""
+            ></div>
+            <div
+              className="h-2.5 w-2.5 rounded-full bg-yellow-500"
+              data-v-241c1152=""
+            ></div>
+            <div
+              className="h-2.5 w-2.5 rounded-full bg-green-500"
+              data-v-241c1152=""
+            ></div>
+          </div>
+          <div className="flex flex-col gap-4 p-4">
+            <div className="mx-auto h-24 w-full max-w-4xl rounded-xl bg-muted/50" />
+            <div className="mx-auto h-64 w-full max-w-4xl rounded-xl bg-muted/50" />
+          </div>
         </div>
       </SidebarInset>
       <SidebarRight />
