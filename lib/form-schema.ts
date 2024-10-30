29adaea5fa91d9d77fc4cf8833b2a6e3b2be16e5
Ouassim Abdelmalek Ghribi <ouassimghribi@gmail.com>
2024-10-30 19:01:53 +0100

    feat: added checkbox zod schema

--- lib/form-schema.ts
+++ lib/form-schema.ts
@@ -19,8 +19,11 @@ export function generateZodSchema(formFields: FormField[]) {
       case FieldType.EMAIL:
         fieldSchema = z.string().email()
         break
+      case FieldType.CHECKBOX:
+        fieldSchema = z.boolean().default(false)
     }
     formSchemaObject[field.name] = fieldSchema as z.ZodTypeAny // Ensure fieldSchema is of type ZodTypeAny
   })
+
   return z.object(formSchemaObject)
 }
