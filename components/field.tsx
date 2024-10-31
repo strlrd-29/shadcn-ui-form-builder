da492b2ed88e97f9379c021db6537a822eaaa353
Ouassim Abdelmalek Ghribi <ouassimghribi@gmail.com>
2024-10-31 22:13:56 +0100

    feat: added dnd kit and sortable form fields in form-editor

--- components/field.tsx
+++ components/field.tsx
@@ -1,29 +1,51 @@
 import * as React from "react"
+import { GripVertical } from "lucide-react"
 import type { UseFormReturn } from "react-hook-form"
 
 import type { FormField as FormFieldType } from "@/types/field"
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
 import { FormField } from "@/components/ui/form"
 import { renderFormFieldComponent } from "@/components/render-form-field-component"
 
-interface FieldProps {
+export interface FieldProps {
   formField: FormFieldType
-  form: UseFormReturn<
+  form?: UseFormReturn<
     {
       [x: string]: any
     },
     any,
     undefined
   >
+  style?: React.CSSProperties
+  isDragging?: boolean
 }
 
-export const Field = React.memo(({ formField, form }: FieldProps) => {
-  return (
-    <FormField
-      control={form.control}
-      name={formField.name}
-      render={({ field }) => renderFormFieldComponent({ field, formField })}
-    />
-  )
-})
+export const Field = React.forwardRef<HTMLDivElement, FieldProps>(
+  ({ formField, form, style, isDragging, ...props }, ref) => {
+    return (
+      <div
+        className={cn("flex items-center gap-2", {
+          "rounded-md bg-muted opacity-60": isDragging,
+        })}
+        style={style}
+        ref={ref}
+      >
+        <div className="w-full">
+          <FormField
+            control={form?.control}
+            name={formField.name}
+            render={({ field }) =>
+              renderFormFieldComponent({ field, formField })
+            }
+          />
+        </div>
+        <Button size="icon" variant="ghost" type="button" {...props}>
+          <GripVertical className="size-4" />
+        </Button>
+      </div>
+    )
+  }
+)
 
 Field.displayName = "Field"
--- components/form-editor.tsx
+++ components/form-editor.tsx
@@ -1,16 +1,30 @@
 "use client"
 
+import * as React from "react"
 import { useFormStore } from "@/stores/form"
+import {
+  DndContext,
+  DragEndEvent,
+  DragOverlay,
+  DragStartEvent,
+} from "@dnd-kit/core"
+import {
+  arrayMove,
+  SortableContext,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { useShallow } from "zustand/shallow"
 
+import { FormField } from "@/types/field"
 import { FormState } from "@/types/form-store"
 import { generateZodSchema } from "@/lib/form-schema"
 import { Button } from "@/components/ui/button"
 import { Form } from "@/components/ui/form"
 import { Field } from "@/components/field"
+import { SortableField } from "@/components/sortable-field"
 
 const selector = (state: FormState) => ({
   formFields: state.formFields,
@@ -18,7 +32,9 @@ const selector = (state: FormState) => ({
 })
 
 export function FormEditor() {
-  const { formFields } = useFormStore(useShallow(selector))
+  const [activeFormField, setActiveFormField] =
+    React.useState<FormField | null>(null)
+  const { formFields, setFormFields } = useFormStore(useShallow(selector))
 
   const formSchema = generateZodSchema(formFields)
 
@@ -30,19 +46,53 @@ export function FormEditor() {
     console.info(values)
   }
 
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event
+
+    if (over && active.id !== over.id) {
+      const oldIndex = formFields.findIndex((field) => field.name === active.id)
+      const newIndex = formFields.findIndex((field) => field.name === over.id)
+
+      setFormFields(arrayMove(formFields, oldIndex, newIndex))
+    }
+    setActiveFormField(null)
+  }
+
+  function handleDragStart(event: DragStartEvent) {
+    const { active } = event
+    const formField = formFields.find((field) => field.name === active.id)
+    if (formField) {
+      setActiveFormField(formField)
+    }
+  }
+
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="mx-auto flex w-3/4 flex-col gap-6 p-4"
-      >
-        {formFields.map((formField) => (
-          <Field formField={formField} form={form} key={formField.name} />
-        ))}
-        <Button type="submit" className="w-fit">
-          Submit
-        </Button>
-      </form>
-    </Form>
+    <DndContext onDragEnd={handleDragEnd} onDragStart={handleDragStart}>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="mx-auto flex w-3/4 flex-col gap-6 p-4"
+        >
+          <SortableContext
+            items={formFields.map((formField) => formField.name)}
+            strategy={verticalListSortingStrategy}
+          >
+            {formFields.map((formField) => (
+              <SortableField
+                formField={formField}
+                form={form}
+                key={formField.name}
+              />
+            ))}
+          </SortableContext>
+          <DragOverlay className="bg-background">
+            {activeFormField ? <Field formField={activeFormField} /> : <></>}
+          </DragOverlay>
+          <Button type="submit" className="w-fit">
+            Submit
+          </Button>
+        </form>
+      </Form>
+    </DndContext>
   )
 }
