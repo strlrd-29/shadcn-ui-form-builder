15458238b818cfbc2bb37bccb3178a8f9f0747c6
Ouassim Abdelmalek Ghribi <ouassimghribi@gmail.com>
2024-11-04 08:53:50 +0100

    feat: open edit-form-field when adding new form field

--- components/sidebar-right.tsx
+++ components/sidebar-right.tsx
@@ -2,7 +2,9 @@
 
 import * as React from "react"
 import { useFormStore } from "@/stores/form"
+import { useShallow } from "zustand/shallow"
 
+import { FormState } from "@/types/form-store"
 import { fields } from "@/lib/constants"
 import {
   Sidebar,
@@ -17,10 +19,17 @@ import {
   SidebarSeparator,
 } from "@/components/ui/sidebar"
 
+const selector = (state: FormState) => ({
+  addFormField: state.addFormField,
+  setIsEditFormFieldOpen: state.setIsEditFormFieldOpen,
+  setSelectedFormField: state.setSelectedFormField,
+})
+
 export function SidebarRight({
   ...props
 }: React.ComponentProps<typeof Sidebar>) {
-  const addFormField = useFormStore((state) => state.addFormField)
+  const { addFormField, setSelectedFormField, setIsEditFormFieldOpen } =
+    useFormStore(useShallow(selector))
   return (
     <Sidebar
       collapsible="none"
@@ -37,13 +46,16 @@ export function SidebarRight({
               {fields.map((field) => (
                 <SidebarMenuItem key={field.name}>
                   <SidebarMenuButton
-                    onClick={() =>
-                      addFormField({
+                    onClick={() => {
+                      const newFormField = {
                         ...field,
                         id: Math.random().toString().slice(-10),
                         name: `${field.name.toLowerCase().replaceAll(" ", "_")}_${Math.random().toString().slice(-10)}`,
-                      })
-                    }
+                      }
+                      addFormField(newFormField)
+                      setSelectedFormField(newFormField.id)
+                      setIsEditFormFieldOpen(true)
+                    }}
                   >
                     <field.Icon />
                     <span>{field.name}</span>
