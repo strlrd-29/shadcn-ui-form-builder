e5741ecffee7e6a73de483f80c2edfe8e1a05add
Ouassim Abdelmalek Ghribi <ouassimghribi@gmail.com>
2024-11-06 21:01:56 +0100

    fix: added metadatabase to layout to fix og image links

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import "./globals.css"
 import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://forms.ouassim.tech"),
   title: {
     default: "Shadcn Form Builder",
     template: "%s - Shadcn Form Builder",
